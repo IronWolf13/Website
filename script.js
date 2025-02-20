bcafe3419216e3b0413d79fe06ce78ccb5142a4a
IronWolf13 <mrblixyo@gmail.com>
2025-02-20 13:06:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -58,21 +58,21 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Configuration Particles.js en mode "bubble"
+// Configuration Particles.js en mode "bubble" avec particules et lignes en blanc
 particlesJS("particles-js", {
   "particles": {
     "number": {
       "value": 80,
       "density": { "enable": true, "value_area": 800 }
     },
-    "color": { "value": "#ff0000" },
+    "color": { "value": "#ffffff" },
     "shape": { "type": "circle" },
     "opacity": { "value": 0.5 },
     "size": { "value": 3, "random": true },
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#ff0000",
+      "color": "#ffffff",
       "opacity": 0.4,
       "width": 1
     },
@@ -105,3 +105,4 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
+
