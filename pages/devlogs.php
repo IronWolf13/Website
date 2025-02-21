a1f17214b503b5626caa3181a5066da5bbf093cd
IronWolf13 <mrblixyo@gmail.com>
2025-02-21 15:06:25 +0100

    Create devlogs.php

---
+++ pages/devlogs.php
@@ -0,0 +1,7 @@
+<h1>Devlogs - Mise à jours</h1>
+<div class="devlogs">
+  <!-- Exemple de boutons menant à une page spécifique pour chaque version -->
+  <button onclick="window.location.href='devlog_v1.php'">Version 1.0</button>
+  <button onclick="window.location.href='devlog_v2.php'">Version 2.0</button>
+  <!-- Ajoutez d'autres boutons selon vos versions -->
+</div>
