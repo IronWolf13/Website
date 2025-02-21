158a015a7f8d2f81cef46d53d8df397fb54db1ae
IronWolf13 <mrblixyo@gmail.com>
2025-02-21 15:05:58 +0100

    Create protocoles.php

---
+++ pages/protocoles.php
@@ -0,0 +1,7 @@
+<h1>Protocole</h1>
+<div class="protocole-section">
+  <div class="section-header" data-target="protocole1">Protocole</div>
+  <div class="section-content" id="protocole1" style="display: block;">
+    <p>Contenu du protocole...</p>
+  </div>
+</div>
