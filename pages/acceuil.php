f4955a3cae96c324a71f1a17fcd7f795e13c1c8e
IronWolf13 <mrblixyo@gmail.com>
2025-02-21 15:05:24 +0100

    Create acceuil.php

---
+++ pages/acceuil.php
@@ -0,0 +1,6 @@
+<h1>Présentation du site</h1>
+<p>
+  Ce site internet est dédié au règlement et aux révocations de sanction de Oméga Facility.<br>
+  Serveur RolePlay moddé sur SCP:SL créé par Le dompteur de raptor et MrPancake.
+</p>
+<!-- Vous pouvez ajouter ici des raccourcis et autres éléments -->
